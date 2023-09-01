0ca1e7995d5a8e3ee24ec280fb19b1e25d43f477
Adam Crampton <adam@adamcrampton.com>
2023-09-01 14:09:45 +1000

    Updated filenames

---
+++ 3_array-types.js
@@ -0,0 +1,29 @@
+// ===========
+// ARRAY TYPES
+// ===========
+// Note: It's important to cast the array type when initializing - to specify what type the array contents should be.
+// Otherwise the array contents will receive an 'any' type.
+var anArray = [];
+// Array of strings
+// ================
+var users = ['Adam', 'Amy'];
+users.push('Maddy');
+// Alternate syntax.
+var altUsers = ['Linus', 'Farfel'];
+altUsers.push('Blanka');
+// Array of numbers
+// ================
+var ages = [3, 12, 16];
+ages.push(22);
+// Alternate syntax.
+var altAges = [4, 13, 17];
+altAges.push(23);
+var coOrds = []; // Must receive an array of "Point" type values.
+coOrds.push({ x: 4, y: 5 });
+// Multidimensional arrays - number of brackets = depth of array
+// =============================================================
+var ticTacToeBoard = [
+    ["X", "O", "X"],
+    ["X", "O", "X"],
+    ["X", "O", "X"]
+];
